55b1dbf429f75d08d1b447436df4071deb4374c4
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-15 19:09:01 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long l;
+	float m;
+
+	printf("The size of a char: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of an int: %lu.\n", (unsigned long)sizeof(j));
+	printf("The size of a long int: %lu.\n", (unsigned long)sizeof(k));
+	printf("The size of a long long int: %lu.\n", (unsigned long)sizeof(l));
+	printf("The size of a float: %lu.\n", (unsigned long)(m));
+
+	return (0);
+}
