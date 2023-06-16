2127b1a747e4a6d831faaaa5fa2b5e513c46d7c8
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-15 18:26:49 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
