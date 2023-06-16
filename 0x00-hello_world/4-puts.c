a8c82cc8306f40ddd70e918caa01d8126068a6d8
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-15 18:11:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
 #include <stdio.h>
 
 int main(void)
