7796b27aa9d57fedf691dc2db9676563873cc2fe
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-16 18:32:36 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+{
+		putchar(letter);
+		letter++;
+}
+	putchar('\n');
+
+	return (0);
+}
+
+
