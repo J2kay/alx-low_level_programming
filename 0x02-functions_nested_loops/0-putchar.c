99a26f89af677932b4012aa76e8980b7b74861dd
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-20 05:17:38 -0700

    orint _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
+
+int main(void)
+{
+
+
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory for storing the codes from task "0x02. C - Functions, nested loops"
