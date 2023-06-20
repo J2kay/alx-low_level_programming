ffb77ea6c46b6d9aa1f03fa25c5f9b6d54ba0af7
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-19 18:00:45 -0700

    print cobo 4

---
+++ 0x01-variables_if_else_while/101-print_combo4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
+
+int main(void)
+{
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
+{
+	for (p = d + 1; p <= '9'; p++)
+{
+	for (q = p + 1; p <= '9'; q++)
+{
+	if ((p != d) != q)
+{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
