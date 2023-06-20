551a65671c3e7cb8b4e461244e742a7c2f8cb0d7
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-20 12:20:39 -0700

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - this the entry point of the program
+ *
+ * print_alphabet - this program prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+
+	print_alphabet();
+	return (0);
+}
