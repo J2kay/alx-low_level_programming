6ac8a265e9c0a8de2b914deb5ff771a8b29ca288
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-22 10:59:15 -0700

    debug positive_or_negative

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This directory was made to practice and learn debugging in the 
+0x03. C - Debugging task
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program.
+ * Description: This function serves as the starting
+ * point of execution when the program is run.
+ * It controls the flow of execution, invokes other
+ * functions, performs calculations,
+ * and interacts with the user.
+ *
+ * Return: 0 on successful execution.
+ */
+int positive_or_negative( int n)
+{
+
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else
+	{
+		printf("error");
+	}
+
+	return (0);
+}
