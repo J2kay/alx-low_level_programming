1f3b4ff0308a0aeac367ff5244c84489fc52b91f
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-25 18:05:41 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("fizzbuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+		printf("\n");
+		return (0);
+}
+
