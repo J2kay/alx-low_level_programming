e10a3d196e988e9176d79afbda217d526dd121d6
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-27 16:41:14 -0700

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,5 +12,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
+
+
 
