164322ebeffcd04f220c630f3851ac767a4b4dfe
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-27 14:56:37 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints the elements of an array
+ * @a: pointer to the firs element of an inputed array
+ * @n: input number of ellements
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (
+
