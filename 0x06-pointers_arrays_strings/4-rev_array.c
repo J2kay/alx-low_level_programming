64dc5a6fe670afa554710fc2e584c4df7534a9ee
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-29 19:58:45 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: input array
+ * @n: in put array index
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, store;
+
+	while (i < n / 2)
+	{
+		store = a[i];
+		a[i] = a[n - i -1];
+		a[n -i -1] = store;
+		i++;
+	}
+}
+
+
+
