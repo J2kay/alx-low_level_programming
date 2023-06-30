c8ebc28d703394e13afd2351e4f2da52974f8446
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-06-29 19:07:43 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	/**int a = 0, b = 0;*/
-	while (*s1 != '\0' && *s2 != '\0')
+	int a = 0, b = 0;
+	while (s1[a] != '\0' && s2[b] == s1[a])
 	{
 		s1++;
 		s2++;
-		if (*s1 > *s2 || *s1 < *s2)
-		{
-		return (-1);
-		}
 	}
-	return (0);
+
+	return (s1[a] - s2[b]);
 }
