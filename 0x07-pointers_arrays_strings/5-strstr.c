f77246271922462562bef9b9631f331186b67cd9
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-03 17:54:55 -0700

    search for first instance of substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,14 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; haystack[i] >= '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] >= '\0'; j++)
-		{
 			if (haystack[i] == needle[0])
 			{
 				return (haystack + i);
 			}
-		}
 	}
 	return ('\0');
 }
