c8c23053dc42a2e7202b3386d5b15fdd98c877c0
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-04 16:05:13 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,16 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
 	unsigned int j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
 
 	for (j = 0; j < n; j++)
 	{
-		src[j] = dest[i] + src[j];
+		dest[j] = src[j];
 	}
 	return (dest);
 }
