9746f64637f9ba08ae9517b24ad4d04fec222919
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-04 10:33:24 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j = 0;
+	int i, j, k;
 
-	for (i = 0; haystack[i] != '\0' && haystack[i] == needle[j]; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for  (j = 0; needle[j] != '\0'; j++)
+		for  (j = 0, k = i; needle[j] != '\0'; j++, k++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[k] != needle[j])
 			{
-				return (haystack + i);
+				break;
 			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
 		}
 	}
 	return ('\0');
