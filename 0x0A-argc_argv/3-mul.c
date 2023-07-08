9da3f7e26ca97cddf06e6f2dff5077263d023c36
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-07 22:37:18 -0700

    multiply arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector array
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+		printf("%d\n", product);
+	}
+	return (0);
+}
