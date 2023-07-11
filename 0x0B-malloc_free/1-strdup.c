aacfde55c5efa58f48cdb1896d9687b832a11267
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-11 13:41:58 -0700

    copy string to new array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string array pointer
+ * Return: pointer to new array
+ */
+
+char *_strdup(char *str)
+{
+	int i, j, len = 0;
+	char *new;
+
+	if (*str == '\0')
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	new = (char *)malloc(sizeof(char) * len);
+	if (new == NULL)
+		return (NULL);
+
+	for (j = 0; j < len; j++)
+	{
+		new[j] = str[j];
+	}
+	return (new);
+}
+
+
