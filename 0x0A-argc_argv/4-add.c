5e800142af81f90d5d738329fa4e8f03c7bbbbbf
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-11 18:05:58 -0700

    add arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,27 +11,24 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0, j;
 
-	if (argc == 1)
+	if (argc <= 1)
 	{
 		printf("0\n");
 		return (1);
 	}
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (argv[i][j] < 48 || argv[i][j]  > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(argv[i]);
 
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j]  > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
