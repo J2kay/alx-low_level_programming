9d8d37f5ec6621451198909e8aaaddc63ae46211
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-13 17:07:14 -0700

    concatinate n elements of s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k, l, len1 = 0, len2 = 0;
 	char *new;
 
-	if (s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		s2 = "";
 	}
