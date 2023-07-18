36a412b02e1cc7cf530110ab6287f2f1e8026f16
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-18 10:22:07 -0700

    dog info function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * init_dog - contains and displays info about a dog
@@ -11,6 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		printf("ok");
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
