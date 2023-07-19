cbee673ba1d3477c68024f9e63e26ee3487160df
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-18 20:12:44 -0700

    free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory allocated to dog
+ * @d: pointer to dog_t
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)	
+			free(d->name);
+		if (d->name != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
