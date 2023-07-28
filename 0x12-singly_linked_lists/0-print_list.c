26f5b4131fb168b21852a2d01f58468f74b105a7
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-27 17:32:22 -0700

    add node to the bottom

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -23,6 +23,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", list->len, list->str);
+		if (list->next == NULL)
+			break;
 		list = list->next;
 		tally++;
 	}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node_end - add a new node to the end
+ * @head: double pointer
+ * @str: input string pointer
+ * Return: end
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *cur, *new_node;
+	int size = 0;
+
+	cur = *head;
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = size;
+
+	if (cur == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (cur->next != NULL)
+	{
+		cur = cur->next;
+	}
+	cur->next = new_node;
+	return (new_node);
+}
