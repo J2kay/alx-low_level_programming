4aa98bf41f4df260f67074c5ec070a6ffb72b2cb
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-07-28 17:03:48 -0700

    constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_first - executes befor the main function
+ */
+
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,4 +24,5 @@ int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print_first(void)__attribute__((constructor));
 #endif
