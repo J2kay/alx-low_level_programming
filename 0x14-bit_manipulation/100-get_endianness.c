2ce4faeee9f13402ad400493cea0d8e92922dab6
j2kari <jeffreyk31k@gmail.com>
2023-08-05 03:20:54 +0000

    task 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: integer
+ *
+ */
+int get_endianness(void)
+{
+    unsigned long int a;
+    char *ptr;
+    ptr = (char *)&a;
+    return ((int)*ptr);
+}
