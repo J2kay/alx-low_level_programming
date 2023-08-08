7f84001b8cba1fcf095e6d374f77347755d7fa69
j2kari <jeffreyk31k@gmail.com>
2023-08-08 02:12:09 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - creates a file and writes content into it
+ * @filename: pointer to a file
+ * @text_content: pointer to a string to be written
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fl, tally = 0, i, len = 0;
+
+    if (filename == NULL)
+        return (-1);
+    fl = open(filename, O_RDWR | O_TRUNC);
+    if (fl == -1)
+        return (-1);
+    if (text_content != NULL)
+    {
+        for (i = 0; text_content[i] != '\0'; i++)
+        {
+            len++;
+        }
+        tally = write(fl, text_content, len);
+        if (tally == -1)
+        {
+            close(fl);
+            return (-1);
+        }
+    }
+    close(fl);
+
+    return (tally);
+}
