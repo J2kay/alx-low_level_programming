4dfb39bbfef2ef58da82bef86fdfbdd49c8125ac
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-08-07 20:58:33 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,27 +7,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fl, tally = 0, i, len = 0;
+	int fl, tally = 0, i;
 
-    if (filename == NULL)
-        return (-1);
-    fl = open(filename, O_RDWR | O_TRUNC);
-    if (fl == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i] != '\0'; i++)
-        {
-            len++;
-        }
-        tally = write(fl, text_content, len);
-        if (tally == -1)
-        {
-            close(fl);
-            return (-1);
-        }
-    }
-    close(fl);
+	if (filename == NULL)
+		return (-1);
+	fl = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fl == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			tally = write(fl, &text_content[i], 1);
+			if (tally == -1)
+			{
+				close(fl);
+				return (-1);
+			}
+		}
+	}
+	close(fl);
 
-    return (tally);
+	return (1);
 }
