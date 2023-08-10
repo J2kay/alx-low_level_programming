094ca858df54d03e8346ecec282c54a2ad4640b3
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-08-10 06:08:27 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,16 +14,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		while (text_content[len] != '\0')
-			len++;
-		tally = write(fd, text_content, len);
-		if (tally == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
+	}
+	while (text_content[len] != '\0')
+		len++;
+	tally = write(fd, text_content, len);
+	if (tally == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
