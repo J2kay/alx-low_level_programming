82b01368423c559e13d61b62092ba30864fb2fff
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-08-31 17:18:21 -0700

    number of bits needed to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - calculates the number of bits
+ *   you would nee to flip to go from n to m
+ * @n: input number
+ * @m: input number
+ * Return: number of flips
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int tally = 0, check = n ^ m;
+
+	while (check)
+	{
+		tally += check & 1;
+		(check) >>= 1;
+	}
+	return (tally);
+}
+
