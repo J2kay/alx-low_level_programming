9341fc234f5907c907df00ed7cb575694ea2cea5
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-11-03 17:35:14 -0700

    task 0 fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
 
 	if (table == NULL)
 		return (NULL);
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
 	table->size = size;
 	return (table);
 }
