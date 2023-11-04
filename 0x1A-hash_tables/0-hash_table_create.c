c25146fa891825f9778b5197c611cad5e29bbec8
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-11-03 17:15:23 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table with the gven size
+ * @size: size of hash table
+ * Return: pointer to newly created table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = NULL;
+	return (table);
+}
